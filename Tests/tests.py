2a9cc3d3930ad9acca687a5c966708e57d35515a
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-07-20 19:50:37 +0100

    Update tests.py

--- Tests/tests.py
+++ Tests/tests.py
@@ -5,7 +5,7 @@ import grade_calculator as gc
 result = gp.func("A", 4)
 print(f"Grade point: {result}")
 
-#For grade_calculator
+#For grade_calculator module.
 while True:
 	math = gc.getInput("Enter your score: ")
 	if 0 <= math <= 100:
