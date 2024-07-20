bef7134eb33ea2cdd06800a2f9a83c146b42c895
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-07-20 19:46:04 +0100

    Create tests.py

---
+++ Tests/tests.py
@@ -0,0 +1,14 @@
+import grade_point as gp
+import grade_calculator as gc
+
+#For grade_point module.
+result = gp.func("A", 4)
+print(f"GP: {result}")
+
+#For grade_calculator
+while True:
+	math = gc.getInput("Enter your score: ")
+	if 0 <= math <= 100:
+		break
+grade = gc.calGrades(math)
+print(f"You had a/an {grade}")
